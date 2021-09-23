82043445ccf95c1d5bb8ab2b496cc7b88482cae1
Ahmed Belal <ahmbelal95@gmail.com>
2021-09-23 04:25:01 +0200

    fix: button recieve props properly

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,7 +1,7 @@
 import styles from './button.module.css'
 
 const Button = ({width, height, children, ...props}) => {
-    return <button style={{width, height}} className={styles['button']}>{children}</button>
+    return <button style={{width, height}} className={styles['button']} {...props}>{children}</button>
 }
 
 export default Button;
