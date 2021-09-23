7f017c07c5ee3075a159ba12089fe7b138758564
Ahmed Belal <ahmbelal95@gmail.com>
2021-09-23 07:54:08 +0200

    chore: building general components

---
+++ src/components/Select/Select.jsx
@@ -0,0 +1,17 @@
+import styles from "./select.module.css";
+
+const Select = ({ options, width, height, ...props }) => {
+  return (
+    <select
+      style={{ width, height }}
+      className={styles["select"]}
+      {...props}
+    >
+        {options.map(option =>{
+            return <option value={option.value}>{option.text}</option>
+        })}
+    </select>
+  );
+};
+
+export default Select;
