8989e3a4320c0b5535a3137d0c8b5d1fef3c3520
Ahmed Belal <ahmbelal95@gmail.com>
2021-09-23 04:37:38 +0200

    feat: button recieves background

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,7 +1,7 @@
 import styles from './button.module.css'
 
-const Button = ({width, height, children, ...props}) => {
-    return <button style={{width, height}} className={styles['button']} {...props}>{children}</button>
+const Button = ({width, height, background,children, ...props}) => {
+    return <button style={{width, height, background}} className={styles['button']} {...props}>{children}</button>
 }
 
 export default Button;
